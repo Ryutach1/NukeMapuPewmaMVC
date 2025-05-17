8832162306090cc1d18e4093513e36427210cad3
Isaac <isaacbaltazarcc@gmail.com>
2025-05-17 00:11:20 -0400

    Inicio de Proyecto MVC nuevo desde 0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoNukeMapuPewmaMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
