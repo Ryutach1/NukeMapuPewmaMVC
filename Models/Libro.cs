220f62aec29732aacd23c5ef4780c7feab682929
CurseHantar <renatopinowo@gmail.com>
2025-05-30 14:26:10 -0400

    Se modificaron los modelos de las clases: Libro y ropa
    Se modificaron los modelos de las clases: Libro y ropa

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -4,7 +4,10 @@ public class Libro
 {
     public int Id { get; set; }
     public required string Nombre { get; set; }
+    public required string Autor { get; set; }
+    public required string Editorial { get; set; }
     public required string Categoria { get; set; }
+    public int Fecha { get; set; }
     public required string Descripcion { get; set; }    
     public decimal Precio { get; set; }
     public int Cantidad { get; set; }
--- Models/Ropa.cs
+++ Models/Ropa.cs
@@ -5,7 +5,6 @@ public class Ropa
     public int Id { get; set; }
     public required string Nombre { get; set; }
     public required string Talla { get; set; }
-    public required string Descripcion { get; set; }    
     public decimal Precio { get; set; }
     public int Cantidad { get; set; }
 }
